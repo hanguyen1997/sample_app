37f7ed9bd627b1e907deca767c23b480671688a8
HaNguyen <you@example.com>
2019-03-06 10:57:37 +0700

    thay doi giao dien

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root 'application#hello'
+  # duong dan trang chu
+ root 'application#hello' 
+
 end
